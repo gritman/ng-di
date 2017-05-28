d84eb0f19c9b1b66f931f0067fc11b7bcbb0c95c
gritman <gritman@126.com>
2017-05-28 11:22:46 +0800

    创建依赖注入示例项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Product1Component } from './product1/product1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Product1Component
   ],
   imports: [
     BrowserModule,
